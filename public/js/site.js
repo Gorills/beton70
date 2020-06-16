c54f2bc732ce65cee5d93bb37532cbd8245e90c4
Gorills <gorivanickiy@gmail.com>
2020-06-16 12:23:46 +0700

    sale

--- public/js/site.js
+++ public/js/site.js
@@ -1,1 +1,190 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var i=t[o]={i:o,l:!1,exports:{}};return e[o].call(i.exports,i,i.exports,n),i.l=!0,i.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var i in e)n.d(o,i,function(t){return e[t]}.bind(null,i));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=40)}({40:function(e,t,n){e.exports=n(41)},41:function(e,t){$(".menu-btn").click((function(e){e.preventDefault(),$(this).toggleClass("menu-btn_active"),$(".header").toggleClass("header--active")})),$(".header__link").click((function(e){$(".menu-btn").removeClass("menu-btn_active"),$(".header").removeClass("header--active")})),$(".show_popup").click((function(e){e.preventDefault(),$(".popup").show(),$(".overlay_popup").show()})),$(".overlay_popup").click((function(){$(".overlay_popup, .popup").hide()})),$("body").on("click",'[href*="#"]',(function(e){$("html,body").stop().animate({scrollTop:$(this.hash).offset().top-120},1e3),e.preventDefault()})),jQuery(document).ready((function(e){var t=document.location.href;e.each(e(".header__link"),(function(){this.href==t&&e(this).addClass("header__link--active")}))})),$(".start__hidden").click((function(e){$(".hidden").toggleClass("hidden--active")}));var n=$(".about").offset().top-$(window).height();$(window).scroll((function(){$(this).scrollTop()>n&&($("#num").animate({num:15},{duration:3e3,step:function(e){this.innerHTML=(e+3).toFixed(0)}}),$("#num2").animate({num:108322},{duration:5e3,step:function(e){this.innerHTML=(e+3).toFixed(0)}}),$("#num3").animate({num:31629},{duration:5e3,step:function(e){this.innerHTML=(e+3).toFixed(0)}}))}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/site.js":
+/*!******************************!*\
+  !*** ./resources/js/site.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(".menu-btn").click(function (e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $(".header").toggleClass('header--active');
+});
+$(".header__link").click(function (e) {
+  $(".menu-btn").removeClass('menu-btn_active');
+  $(".header").removeClass('header--active');
+});
+$('.show_popup').click(function (e) {
+  // Вызываем функцию по нажатию на кнопку
+  e.preventDefault();
+  $('.popup').show(); // Открываем блок заднего фона
+
+  $('.overlay_popup').show(); // Открываем блок заднего фона
+});
+$('.overlay_popup').click(function () {
+  // Обрабатываем клик по заднему фону
+  $('.overlay_popup, .popup').hide(); // Скрываем затемнённый задний фон и основное всплывающее окно
+});
+$("body").on('click', '[href*="#"]', function (e) {
+  var fixed_offset = 120;
+  $('html,body').stop().animate({
+    scrollTop: $(this.hash).offset().top - fixed_offset
+  }, 1000);
+  e.preventDefault();
+});
+jQuery(document).ready(function ($) {
+  var url = document.location.href;
+  $.each($(".header__link"), function () {
+    if (this.href == url) {
+      $(this).addClass('header__link--active');
+    }
+  });
+});
+$(".start__hidden").click(function (e) {
+  // e.preventDefault();
+  $('.hidden').toggleClass('hidden--active');
+});
+var target = $('.about');
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+$(window).scroll(function () {
+  var winScrollTop = $(this).scrollTop();
+
+  if (winScrollTop > scrollToElem) {
+    $('#num').animate({
+      num: 18 - 3
+      /* - начало */
+
+    }, {
+      duration: 3000,
+      step: function step(num) {
+        this.innerHTML = (num + 3).toFixed(0);
+      }
+    });
+    $('#num2').animate({
+      num: 108325 - 3
+      /* - начало */
+
+    }, {
+      duration: 5000,
+      step: function step(num) {
+        this.innerHTML = (num + 3).toFixed(0);
+      }
+    });
+    $('#num3').animate({
+      num: 31632 - 3
+      /* - начало */
+
+    }, {
+      duration: 5000,
+      step: function step(num) {
+        this.innerHTML = (num + 3).toFixed(0);
+      }
+    });
+  }
+});
+
+/***/ }),
+
+/***/ 1:
+/*!************************************!*\
+  !*** multi ./resources/js/site.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/gorills/project/beton70/resources/js/site.js */"./resources/js/site.js");
+
+
+/***/ })
+
+/******/ });
